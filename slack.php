df9fe8dc9185024849cdbc8f80e3bbb7901113f6
ojeaga union <unionolivia@gmail.com>
2018-04-11 19:20:36 +0100

    updated

---
+++ slack.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First HNG Internship Task</title>
+	<style type="text/css">
+	body{
+
+		background-image: url(images/slack.png);
+		background-size: cover;
+		background-repeat: repeat-x;
+}
+h2{
+position:absolute;
+
+margin:auto;
+
+top:0;
+
+left:0;
+
+right:0;
+
+bottom:0;
+
+height:fit-content;
+
+width:fit-content;
+}
+</style>
+	
+</head>
+<body>
+ <center>
+
+ <h2>
+ </br>
+ HNG Internship 
+</br> developing great minds
+</br> Guess what??
+
+</br>
+<?php
+echo "The time is" .date ("h:i:sa");
+?>
+</h2>
+</center>
+
+</body>
+</html>
